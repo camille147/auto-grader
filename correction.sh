25f9f4857212964cc1e7469a7e66763c64078baf
manille <manilledesseaux@gmail.com>
2025-04-25 14:58:40 +0200

    check_and_play_fonction

--- correction.sh
+++ correction.sh
@@ -19,9 +19,29 @@ create_grades_csv() {
 	fi
 }
 
+check_and_play_student_program() {
+	if find . -name "Makefile"; then
+		echo "lancement du makefile"
+		make all
+		./factorielle 5
+		echo "executable lancé avec succès."
+	else
+		echo "Le projet ne dispose pas de fichier Makefile"
+	fi
+}
+
+check_factorial() {
+	if [ "$number" -ge 1 -a "$number" -le 10 ]; then
+		echo "C'est une factoriel entre 1 et 10"
+	else
+		echo "Ce n'est pas une factoriel"
+	fi
+}
+
 main() {
 	check_student_project
 	create_grades_csv
+	check_and_play_student_program
 }
 
 main "${@}"
